c10e95d9490d1ae618e4e7a813da559644b2914b
990711 <s18331@sci.pdn.ac.lk>
2023-11-04 11:44:48 +0530

    First test component in Rider

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <p>I am Anushka ..</p>
+      
     </>
   )
 }
---
+++ src/Components/Rider_Test_Component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Rider_Test_Component() {
+  return (
+    <div>
+      <h1>This is test Rider..</h1>
+    </div>
+  )
+}
