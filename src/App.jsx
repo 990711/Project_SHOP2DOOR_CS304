04317efdde6e8860b49c3ed4141ab9f924ac1494
shop_owner_backend <you@example.com>
2023-11-11 21:00:47 +0530

    started Page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import ProductListing from './Pages/ShopOwner/ShopOwner_ProductManagement/ProductListing';
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-    
-    </>
+    <div>
+      <ProductListing />
+    </div>
   );
-}
+};
 
 export default App;
