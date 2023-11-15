82d0eb97cad0d30f9d80df49e2eee8d8ead5b8f2
990711 <s18331@sci.pdn.ac.lk>
2023-11-15 19:38:37 +0530

    Login and Role tables joined. Repeated entries to be handled.

--- src/main/java/com/shop/model/Login.java
+++ src/main/java/com/shop/model/Login.java
@@ -1,8 +1,11 @@
 package com.shop.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +22,21 @@ public class Login {
 	@Column(name="password")
 	private String password;
 	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="fk_role_id")
+	private Role role;
+	
+	
+	
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 	public Login() {
 		
 	}
