9f828cfc63d0fd3579f5ecf2d2b85b26a443d8ff
990711 <s18331@sci.pdn.ac.lk>
2023-11-16 15:42:20 +0530

    Join removed and make user id an auto-generated value.

--- src/main/java/com/shop/Shop1Application.java
+++ src/main/java/com/shop/Shop1Application.java
@@ -13,3 +13,4 @@ public class Shop1Application {
 	}
 
 }
+
--- src/main/java/com/shop/model/Login.java
+++ src/main/java/com/shop/model/Login.java
@@ -1,11 +1,13 @@
 package com.shop.model;
 
-import jakarta.persistence.CascadeType;
+//import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 public class Login {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int user_id;
 	
 	@Column(name="username")
@@ -22,20 +25,6 @@ public class Login {
 	@Column(name="password")
 	private String password;
 	
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="fk_role_id")
-	private Role role;
-	
-	
-	
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
 
 	public Login() {
 		
