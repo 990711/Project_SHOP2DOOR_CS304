f0d840d232247684694370dfdf1f6a73b25a4a9c
990711 <s18331@sci.pdn.ac.lk>
2023-11-22 23:06:20 +0530

    Shop owner job posting controller class added.

---
+++ src/main/java/com/shop/controller/ShopOwnerJobController.java
@@ -0,0 +1,81 @@
+package com.shop.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.exception.ResourceNotFound;
+import com.shop.model.ShopOwnerJob;
+import com.shop.repositary.ShopOwnerJobRepo;
+
+//@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class ShopOwnerJobController {
+
+	@Autowired
+    private ShopOwnerJobRepo shopOwner_jobPostingsRepository;
+
+    // Get all job postings
+    @GetMapping("/shopowner_jobpostings")
+    public List<ShopOwnerJob> getAllJobPostings() {
+        return shopOwner_jobPostingsRepository.findAll();
+    }
+
+    // Create a new job posting
+    @PostMapping("/shopowner_jobpostings")
+    public ShopOwnerJob createJobPosting(@RequestBody ShopOwnerJob shopowner_jobpostings) {
+        return shopOwner_jobPostingsRepository.save(shopowner_jobpostings);
+    }
+
+    // Get a job posting by ID
+    @GetMapping("/shopowner_jobpostings/{id}")
+    public ResponseEntity<ShopOwnerJob> getJobPostingById(@PathVariable Long id) {
+    	ShopOwnerJob shopowner_jobpostings = shopOwner_jobPostingsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound("Job Posting not found with id: " + id));
+        return ResponseEntity.ok(shopowner_jobpostings);
+    }
+
+    // Update a job posting by ID
+    @PutMapping("/shopowner_jobpostings/{id}")
+    public ResponseEntity<ShopOwnerJob> updateJobPosting(@PathVariable Long id, @RequestBody ShopOwnerJob jobPostingsDetails) {
+    	ShopOwnerJob shopowner_jobpostings = shopOwner_jobPostingsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound("Job Posting not found with id: " + id));
+        
+   
+    	shopowner_jobpostings.setJobTitle(jobPostingsDetails.getJobTitle());
+    	shopowner_jobpostings.setDescription(jobPostingsDetails.getDescription());
+    	shopowner_jobpostings.setApplicationDeadline(jobPostingsDetails.getApplicationDeadline());
+    	shopowner_jobpostings.setApplicationStatus(jobPostingsDetails.getApplicationStatus());
+ 
+        
+        // Add more fields to update as needed
+        
+        ShopOwnerJob updatedJobPosting = shopOwner_jobPostingsRepository.save(shopowner_jobpostings);
+        return ResponseEntity.ok(updatedJobPosting);
+    }
+
+    // Delete a job posting by ID
+    @DeleteMapping("/shopowner_jobpostings/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteJobPosting(@PathVariable Long id) {
+    	ShopOwnerJob shopowner_jobpostings = shopOwner_jobPostingsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound("Job Posting not found with id: " + id));
+        
+        shopOwner_jobPostingsRepository.delete(shopowner_jobpostings);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+	
+}
--- src/main/java/com/shop/repositary/ShopOwnerJobRepo.java
+++ src/main/java/com/shop/repositary/ShopOwnerJobRepo.java
@@ -3,7 +3,10 @@ package com.shop.repositary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.shop.model.ShopOwnerJob;
+
 @Repository
-public interface ShopOwnerJob extends JpaRepository<ShopOwnerJob, Long> {
+public interface ShopOwnerJobRepo extends JpaRepository<ShopOwnerJob, Long> {
 
 }
+
