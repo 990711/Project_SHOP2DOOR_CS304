f4ff395af8b7f3614b71caa34639d964c969fe5e
shop_owner_backend <you@example.com>
2023-11-26 16:39:22 +0530

    conflicts resolving

--- src/main/java/com/shop/controller/LoginController.java
+++ src/main/java/com/shop/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.shop.controller;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-//package net.javaguides.springboot.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,42 +16,94 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.shop.exception.ResourceNotFoundException;
+import com.shop.exception.ResourceNotFound;
 import com.shop.model.Login;
-import com.shop.repository.LoginRepo;
+import com.shop.repositary.LoginRepo;
+import com.shop.service.LoginService;
+import com.shop.service.SmsService;
 
-//import net.javaguides.springboot.model.Product;
+import jakarta.validation.Valid;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
-
 public class LoginController {
+
+	@Autowired
+	private LoginRepo loginRepo;
+
+	@Autowired
+	private LoginService service;
 	
 	@Autowired
-    private LoginRepo loginRepo;
-    
-    /*
-    // Get all users
-    @GetMapping("/users")
-    public List<Login> getAllUsers() {
-        return loginRepo.findAll();
-    }
+	private SmsService smsservice;
 
-    
-    */
+	// Get Riders
+	@GetMapping("/LoginDetails")
+	public List<Login> getAllRiders() {
+		smsservice.sendSMS("+94714064457", "this is the message");
+		return loginRepo.findAll();
+	}
 
-    /*
-    // Create user rest api
-    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Login createuser(@RequestBody Login login) {
-        return loginRepo.save(login);
+	// Add Rider
+	@PostMapping("/LoginDetails")
+	public ResponseEntity<Login> addUser(@Valid @RequestBody Login user) {
+
+		Login savedLogin = service.createLogin(user);
+		return new ResponseEntity<Login>(savedLogin, HttpStatus.CREATED);
+	}
+	
+	
+	@DeleteMapping("/LoginDetails/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable int id){
+		Login login = loginRepo.findById(id).orElseThrow(()-> new ResourceNotFound("Rider does not exist with id "+id));
+		login.setDeleted(true);
+		loginRepo.save(login);
+		String msg = "User successfully deleted!";
+		return ResponseEntity.ok(msg);
+	}
+	
+	@PostMapping("/Login")
+    public ResponseEntity<String> handleLogin(@RequestBody Login login) {
+        // Extract username and password from the request
+        String username = login.getUsername();
+        String password = login.getPassword();
+        String role = login.getRole();
+
+        // Check if the user exists
+        
+        Optional<Login> existingUser = loginRepo.findByUsername(username);
+        
+        if (existingUser.isPresent()) {
+        	if(existingUser.get().getPassword().equals(password))
+        	{
+        		if(existingUser.get().getRole().equals(role))
+        		{
+        			return ResponseEntity.ok("Login successful!");
+        		}
+        		else {
+                    // Authentication failed
+                    // Return a ResponseEntity indicating failure
+                    return ResponseEntity.status(401).body("Role mismatch");
+                }
+        	}else {
+                // Authentication failed
+                // Return a ResponseEntity indicating failure
+                return ResponseEntity.status(401).body("Password mismatch");
+            }
+        }
+        else {
+            // Authentication failed
+            // Return a ResponseEntity indicating failure
+            return ResponseEntity.status(401).body("User not found");
+        }
+	
     }
-    
-    */
- // Create user rest api
-    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
+	
+	
+	// Create user rest api
+    @PostMapping("/checkUsername")
     public ResponseEntity<Login> createuser(@RequestBody Login login) {
         // Check for duplicate usernames
     	Optional<Login> existingUser = loginRepo.findByUsername(login.getUsername());
@@ -72,144 +122,15 @@ public class LoginController {
         
     }
     
+    @PutMapping("/Logout/{id}")
+	public ResponseEntity<String> Logout(@PathVariable int id){
+		Login login = loginRepo.findById(id).orElseThrow(()-> new ResourceNotFound("Rider does not exist with id "+id));
+		login.setActive(false);
+		loginRepo.save(login);
+		String msg = "User successfully logged out!";
+		return ResponseEntity.ok(msg);
+	}
     
     
-    /*
-    
-    @PostMapping(value = "/userlogin", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> handleLogin(@RequestBody Login login) {
-        // Extract username and password from the request
-        String username = login.getUsername();
-        String password = login.getPassword();
-        
-
-
-        // Perform necessary validation on username and password
-        if (username == null || password == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username and password are required.");
-        }
-
-        // Check if the user exists
-        
-        Optional<Login> existingUser = loginRepo.findByUsername(username);
-        
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(password) ) {
-        	//String userRole = existingUser.get().getRole();
-            //return ResponseEntity.ok().body(Collections.singletonMap("userRole", userRole));
-            return ResponseEntity.ok().build();
-        } else {
-            // Authentication failed
-            // Return a ResponseEntity indicating failure
-            return ResponseEntity.status(401).build();
-        }
-    }
-*/
-    
-    @PostMapping(value = "/userlogin", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> handleLogin(@RequestBody Login login) {
-        // Extract username and password from the request
-        String username = login.getUsername();
-        String password = login.getPassword();
-        String role = login.getRole();
-
-
-        // Perform necessary validation on username and password
-        if (username == null || password == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username and password are required.");
-        }
-
-        // Check if the user exists
-        
-        Optional<Login> existingUser = loginRepo.findByUsername(username);
-        
-        if (existingUser.isPresent()) {
-        	if(existingUser.get().getPassword().equals(password))
-        	{
-        		if(existingUser.get().getRole().equals(role))
-        		{
-        			return ResponseEntity.ok().build();
-        		}
-        		else {
-                    // Authentication failed
-                    // Return a ResponseEntity indicating failure
-                    return ResponseEntity.status(401).build();
-                }
-        	}else {
-                // Authentication failed
-                // Return a ResponseEntity indicating failure
-                return ResponseEntity.status(401).build();
-            }
-        }
-        else {
-            // Authentication failed
-            // Return a ResponseEntity indicating failure
-            return ResponseEntity.status(401).build();
-        }
-	
-    }
-    
-    
-    
-    /*
-    
-    
-    if (existingUser.isPresent() && existingUser.get().getPassword().equals(password) && existingUser.get().getRole().equals(role)) {
-    	//serRole = existingUser.get().getRole();
-            //return ResponseEntity.ok().body(Collections.singletonMap("userRole", userRole));
-            return ResponseEntity.ok().build();
-    }
-    */
-    
-    
-
-
-    
-    /*
-
-    // Get user by id rest api
-    @GetMapping("/users/{id}")
-    public ResponseEntity<Login> getUserByUserName(@PathVariable String username) {
-    	Login login = loginRepo.findByUsername(username)
-                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with name: " + username));
-        return ResponseEntity.ok(login);
-    }
-    
- // Get user by username rest api
-    @GetMapping("/users/{username}")
-    public ResponseEntity<Login> getUserById(@PathVariable Long id) {
-    	Login login = loginRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with id: " + id));
-        return ResponseEntity.ok(login);
-    }
-    
-    
-
-    // Update product rest api
-    @PutMapping("/users/{id}")
-    public ResponseEntity<Login> updateUserById(@PathVariable Long id, @RequestBody Login userDetails) {
-    	Login login = loginRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("user does not exist with id: " + id));
-
-    	login.setUsername(userDetails.getUsername());
-    	login.setPassword(userDetails.getPassword());
-    	login.setRole(userDetails.getRole());
-        
-
-    	Login updatedUser = loginRepo.save(login);
-        return ResponseEntity.ok(updatedUser);
-    }
-
-    // Delete product rest api
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
-    	Login login = loginRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with id: " + id));
-
-    	loginRepo.delete(login);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
-    }
-    */
 
 }
--- src/main/java/com/shop/model/Login.java
+++ src/main/java/com/shop/model/Login.java
@@ -1,47 +1,67 @@
 package com.shop.model;
 
-//import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.OneToOne;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Login")
-
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Login {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private int user_id;
 
+	@NotBlank(message = "this column must be filled!")
 	@Column(name = "username")
 	private String username;
 
+	@NotBlank(message = "this column must be filled!")
+	@Size(min = 8, message = "password must be greater than 8 charactors!")
 	@Column(name = "password")
 	private String password;
 
 
-	@Column(name = "role")
+
+	@NotBlank(message = "must be select!")
 	private String Role;
 	
+	@Column(name = "active")
+	private boolean active = true;
 	
-
+	@Column(name = "blocked")
+	private boolean blocked = false;
+	
+	@Column(name = "deleted")
+	private boolean deleted = false;
+	
 	public Login() {
-    }
-
-	public Login(String username, String password, String Role) {
-		this.username = username;
-		this.password = password;
-		this.Role = Role;
 
 	}
+	
+	
+
+	public Login(@NotBlank(message = "this column must be filled!") String username,
+			@NotBlank(message = "this column must be filled!") @Size(min = 8, message = "password must be greater than 8 charactors!") String password,
+			@NotBlank(message = "must be select!") String role, boolean active, boolean blocked, boolean deleted) {
+		super();
+		this.username = username;
+		this.password = password;
+		Role = role;
+		this.active = active;
+		this.blocked = blocked;
+		this.deleted = deleted;
+	}
+
+
 
 	public String getRole() {
 		return Role;
@@ -49,14 +69,6 @@ public class Login {
 
 	public void setRole(String role) {
 		Role = role;
-	}
-
-	public Long getUser_id() {
-		return id;
-	}
-
-	public void setUser_id(Long user_id) {
-		this.id = user_id;
 	}
 
 	public String getUsername() {
@@ -75,6 +87,32 @@ public class Login {
 		this.password = password;
 	}
 
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	
+	
+	
 	
 
 }
