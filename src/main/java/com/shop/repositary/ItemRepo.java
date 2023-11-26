1bb8b6040de7e0c9d705072841f032447dcc9a73
990711 <s18331@sci.pdn.ac.lk>
2023-11-26 11:43:40 +0530

    Item controller added,

--- src/main/java/com/shop/repositary/ItemRepo.java
+++ src/main/java/com/shop/repositary/ItemRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.shop.model.Item;
 
 @Repository
-public interface ItemRepo extends JpaRepository<Item, Integer>{
+public interface ItemRepo extends JpaRepository<Item, Long>{
 
 }
