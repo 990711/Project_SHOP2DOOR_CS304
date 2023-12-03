cd8355b088b7c1a1a341c45a4d2a945128db600a
990711 <s18331@sci.pdn.ac.lk>
2023-12-03 22:00:56 +0530

    Customer module CRUD completed

--- src/main/java/com/shop/controller/CustomerController.java
+++ src/main/java/com/shop/controller/CustomerController.java
@@ -6,13 +6,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shop.exception.ResourceNotFound;
 import com.shop.model.Customer;
+import com.shop.model.Item;
 import com.shop.model.Login;
 import com.shop.model.Order;
 import com.shop.repositary.CustomerRepo;
@@ -36,10 +41,11 @@ public class CustomerController {
 	private LoginRepo loginRepo;
 	
 	// Add Customer
-		@PostMapping("/CustomerDetails")
+		@PostMapping("/Customer")
 		public ResponseEntity<String> addCustomer(@Valid @RequestBody Customer customer) {
 
-			Optional<Login> existingUser = loginRepo.findByUsername(customer.getUsername());
+			//Optional<Login> existingUser = loginRepo.findByUsername(customer.getUsername());
+			Optional<Customer> existingUser = customerRepo.findByUsername(customer.getUsername());
 			
 			if (existingUser.isPresent()) {
 				return ResponseEntity.status(401).body("Please find another username..");
@@ -55,6 +61,40 @@ public class CustomerController {
 		@GetMapping("/Customer")
 		public List<Customer> getCustomers() {
 			return customerRepo.findAll();
+		}
+		
+		@GetMapping("/Customer/{username}")
+	    public ResponseEntity<Customer> getCustomerByUsername(@PathVariable String username) {
+	    	Customer customer = customerRepo.findByUsername(username)
+	                .orElseThrow(() -> new ResourceNotFound("Customer not found with username: " + username));
+	        return ResponseEntity.ok(customer);
+	 }
+		
+		@PutMapping("/Customer/{username}")
+	    public ResponseEntity<Customer> updateCustomerByUsername(@PathVariable String username, @RequestBody Customer newCustomer) {
+	    	Customer customer = customerRepo.findByUsername(username)
+	                .orElseThrow(() -> new ResourceNotFound("Customer not found with username: " + username));
+	        
+	    	customer.setAddress(newCustomer.getAddress());
+	    	customer.setEmail(newCustomer.getEmail());
+	    	customer.setName(newCustomer.getName());
+	    	customer.setPassword(newCustomer.getPassword());
+	    	customer.setPhone(newCustomer.getPhone());
+	    	customer.setUsername(newCustomer.getUsername());
+	        
+	        Customer updatedCustomer = customerRepo.save(customer);
+	        return ResponseEntity.ok(updatedCustomer);
+	    }
+		
+		@DeleteMapping("/Customer/{username}")
+		public ResponseEntity<String> deleteCustomer(@PathVariable String username){
+			Customer customer = customerRepo.findByUsername(username)
+	                .orElseThrow(() -> new ResourceNotFound("Customer not found with username: " + username));
+	        
+			customer.setDeleted(true);
+			customerRepo.save(customer);
+			String msg = "Customer successfully deleted!";
+			return ResponseEntity.ok(msg);
 		}
 	
 }
