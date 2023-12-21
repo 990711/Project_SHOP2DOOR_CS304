83322a24a141393a6ba9ab8f69cea6c6e37a2dd0
990711 <s18331@sci.pdn.ac.lk>
2023-12-21 23:17:24 +0530

    Cross Configuration class added.

---
+++ src/main/java/com/shop/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.shop;
+
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**") // Allow CORS for all endpoints
+        .allowedOrigins("http://localhost:5173") // Allow requests from this origin
+        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+        .allowedHeaders("*"); // Allow all headers
+}
+}
