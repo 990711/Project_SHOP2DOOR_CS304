9e7f14ea84776428522010d2400434ccc0759bb6
Dinesh <jeewanthadiensh1@gmail.com>
2024-01-19 20:22:48 +0530

    created a enpont in shopownercontroller to retrieve the order details belongs to a particular shopowner

--- src/main/java/com/shop/repositary/ItemQuantityRepo.java
+++ src/main/java/com/shop/repositary/ItemQuantityRepo.java
@@ -1,6 +1,7 @@
 package com.shop.repositary;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,8 @@ import com.shop.model.ItemQuantityKey;
 public interface ItemQuantityRepo extends JpaRepository<ItemQuantity, ItemQuantityKey> {
 
 	@Query(value = "SELECT item_id,quantity FROM item_quantity WHERE order_id = :id", nativeQuery = true)
-    List<Object[]> findItemsByOrderID(@Param("id") long id);
-	
+	List<Object[]> findItemsByOrderID(@Param("id") long id);
+
+	@Query(value = "SELECT iq.order_id,iq.quantity,itm.name,itm.brand,dr.name FROM shop.item_quantity iq INNER JOIN shop.item itm ON iq.shop_id = itm.shop_id  INNER JOIN shop.orders odr ON iq.order_id = odr.order_id INNER JOIN shop.delivery_rider dr ON odr.delivery_id = dr.user_id WHERE iq.shop_id = :id", nativeQuery = true)
+	List<Map<String, Object>> findItemsByShopOwnerID(@Param("id") int id);
 }
